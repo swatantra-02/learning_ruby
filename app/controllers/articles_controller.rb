8904b6a09c2a00b98d86c11cdbc2800e452e9da1
Swatantra <swatantra827@gmail.com>
2024-09-02 00:08:13 +0530

    CRUD operation completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,45 @@
 class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
+        if @article.errors.any?
+            render json: @article.errors.full_messages
+        else 
+            render json: @article
+        end
+    end
+    def index
+        @articles = Article.all
+        render json: @articles
+    end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+          flash[:notice]="Article is successfully created !"
+          redirect_to @article 
+        else
+          render 'new'
+        end
+        render json: @articles
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        @article.update(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice]="Article is successfully edited !"
+            redirect_to @article 
+        else
+            render 'edit'
+        end
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 end
